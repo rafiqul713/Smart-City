139b576de05ad374bcc161988dc4ed9780ced9a8
Rafiqul Islam <rafiqul.islam@s2018.tu-chemnitz.de>
2020-08-16 00:47:00 +0200

    rest api: v1

--- SLMS/api/serializers.py
+++ SLMS/api/serializers.py
@@ -3,6 +3,13 @@ from .models import StreetLight
 
 
 class StreetLightSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = StreetLight
-        fields = ("light_status", "comment")
+    model = StreetLight
+    fields = ("light_status", "comment")
+    def create(self, validated_data):
+        return StreetLight.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.light_status = validated_data.get('light_status',instance.light_status)
+        instance.comment = validated_data.get('comment', instance.comment)
+        instance.save()
+        return instance
--- SLMS/api/views.py
+++ SLMS/api/views.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from api.models import StreetLight
 from api.serializers import StreetLightSerializer
+from django.http import HttpResponse
 
-# Create your views here.
+
+@api_view(['GET', 'POST'])
+def light_status(request):
+    if request.method == 'GET':
+        #print("Request data ",request.method)
+        obj = StreetLight.objects.all()
+        # serializer= StreetLightSerializer(obj)
+        print("Serializer ",obj)
+        # return Response(serializer.data)
+        return HttpResponse("Testing response")
+
+    elif request.method == 'POST':
+        return HttpResponse("Testing response")
+        # serializer = StreetLightSerializer(data=request.data)
+        # if serializer.is_valid():
+        #     serializer.save()
+        #     return Response(serializer.data, status=status.HTTP_201_CREATED)
+        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
